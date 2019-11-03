941e368512371fba850105e1beb0b425ae03649d
Kevin Higgs <github@kevinhiggs.com>
2019-11-03 13:27:37 -0500

    fix indentation

--- archive/2019/fun/rookie.py
+++ archive/2019/fun/rookie.py
@@ -4,11 +4,11 @@ n,q=f()
 l=[0]*i(n)
 m=[*map(i,f())]
 for _ in [0]*i(q):
-a,b,*c=f();b=i(b)-1
-if a=='FILL':l[b]=m[b]
-if a=='POUR':
-c=i(c[0])-1;d=m[c]-l[c]
-if l[b]<d:l[c]+=l[b];l[b]=0
-else:l[b]-=d;l[c]+=d
-if a=='EMPTY':l[b]=0
+	a,b,*c=f();b=i(b)-1
+	if a=='FILL':l[b]=m[b]
+	if a=='POUR':
+		c=i(c[0])-1;d=m[c]-l[c]
+		if l[b]<d:l[c]+=l[b];l[b]=0
+		else:l[b]-=d;l[c]+=d
+	if a=='EMPTY':l[b]=0
 print(*l)
